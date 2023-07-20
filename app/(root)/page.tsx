492b9be8e991720bbc919aafd0852f0160b12644
Rossrival Moises Bendillo <116779772+Kashea@users.noreply.github.com>
2023-07-20 10:49:38 +0800

    update store-modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
+import { useEffect } from "react";
 
-export default function Home() {
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
     return (
       <div className="p-4">
-        <Button>Click Me</Button>
+        Root Page
       </div>
-    )
+    );
   }
+
+
+  export default SetupPage;
   
